4a50d4dd3400eff32082169514a47e58272d99ee
Joel K Rivers <48956906+aTurbulentMind@users.noreply.github.com>
2025-06-10 12:03:38 -0700

    Add files via upload

--- src/routes/World/LandSet.svelte
+++ src/routes/World/LandSet.svelte
@@ -4,20 +4,7 @@
 	import { interactivity, ContactShadows } from '@threlte/extras';
 	import { Spring } from 'svelte/motion';
 
-	const gltfLand = useLoader(GLTFLoader).load('/src/lib/assets/models/World/HomeBoundLand.glb');
-	const gltfLandB = useLoader(GLTFLoader).load('/src/lib/assets/models/World/HomeBoundLandBot.glb');
-	const gltfStreet = useLoader(GLTFLoader).load('/src/lib/assets/models/World/HomeBoundStreet.glb');
-	const gltfParking = useLoader(GLTFLoader).load(
-		'/src/lib/assets/models/World/HomeBoundParking.glb'
-	);
-	const gltfTrail = useLoader(GLTFLoader).load('/src/lib/assets/models/World/HomeBoundTrail.glb');
-	const gltfExtras = useLoader(GLTFLoader).load('/src/lib/assets/models/World/HomeBoundExtras.glb');
-	const gltfBuildings = useLoader(GLTFLoader).load(
-		'/src/lib/assets/models/World/HomeBoundBuildings.glb'
-	);
-	const gltfNeighborhood = useLoader(GLTFLoader).load(
-		'/src/lib/assets/models/World/HomeBoundNeighborhood.glb'
-	);
+	const gltfLand = useLoader(GLTFLoader).load('World/HomeBoundLand.glb');
 
 	interactivity();
 
@@ -26,32 +13,4 @@
 
 {#if $gltfLand}
 	<T is={$gltfLand.scene} position={[10, 0, 0]} scale={scale.current} castShadow />
-{/if}
-
-{#if $gltfLandB}
-	<T is={$gltfLandB.scene} position={[10, 0, 0]} scale={scale.current} castShadow />
-{/if}
-
-{#if $gltfStreet}
-	<T is={$gltfStreet.scene} position={[10, 0, 0]} scale={scale.current} castShadow />
-{/if}
-
-{#if $gltfParking}
-	<T is={$gltfParking.scene} position={[10, 0, 0]} scale={scale.current} castShadow />
-{/if}
-
-{#if $gltfTrail}
-	<T is={$gltfTrail.scene} position={[10, 0, 0]} scale={scale.current} castShadow />
-{/if}
-
-{#if $gltfExtras}
-	<T is={$gltfExtras.scene} position={[10, 0, 0]} scale={scale.current} castShadow />
-{/if}
-
-{#if $gltfBuildings}
-	<T is={$gltfBuildings.scene} position={[10, 0, 0]} scale={scale.current} castShadow />
-{/if}
-
-{#if $gltfNeighborhood}
-	<T is={$gltfNeighborhood.scene} position={[10, 0, 0]} scale={scale.current} castShadow />
 {/if}
