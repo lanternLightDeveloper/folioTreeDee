4cdc5fc23db3d18730fa23df62241cc2b76db816
Joel K Rivers <48956906+aTurbulentMind@users.noreply.github.com>
2025-06-10 12:05:43 -0700

    Add files via upload

--- src/routes/World/LandSet.svelte
+++ src/routes/World/LandSet.svelte
@@ -5,6 +5,14 @@
 	import { Spring } from 'svelte/motion';
 
 	const gltfLand = useLoader(GLTFLoader).load('World/HomeBoundLand.glb');
+	const gltfLandB = useLoader(GLTFLoader).load('World/HomeBoundLandBot.glb');
+	const gltfStreet = useLoader(GLTFLoader).load('World/HomeBoundStreet.glb');
+	const gltfParking = useLoader(GLTFLoader).load('World/HomeBoundParking.glb');
+	const gltfTrail = useLoader(GLTFLoader).load('World/HomeBoundTrail.glb');
+	const gltfExtras = useLoader(GLTFLoader).load('World/HomeBoundExtras.glb');
+	const gltfBuildings = useLoader(GLTFLoader).load('World/HomeBoundBuildings.glb');
+	const gltfNeighborhood = useLoader(GLTFLoader).load('World/HomeBoundNeighborhood.glb');
+	);
 
 	interactivity();
 
@@ -13,4 +21,32 @@
 
 {#if $gltfLand}
 	<T is={$gltfLand.scene} position={[10, 0, 0]} scale={scale.current} castShadow />
+{/if}
+
+{#if $gltfLandB}
+	<T is={$gltfLandB.scene} position={[10, 0, 0]} scale={scale.current} castShadow />
+{/if}
+
+{#if $gltfStreet}
+	<T is={$gltfStreet.scene} position={[10, 0, 0]} scale={scale.current} castShadow />
+{/if}
+
+{#if $gltfParking}
+	<T is={$gltfParking.scene} position={[10, 0, 0]} scale={scale.current} castShadow />
+{/if}
+
+{#if $gltfTrail}
+	<T is={$gltfTrail.scene} position={[10, 0, 0]} scale={scale.current} castShadow />
+{/if}
+
+{#if $gltfExtras}
+	<T is={$gltfExtras.scene} position={[10, 0, 0]} scale={scale.current} castShadow />
+{/if}
+
+{#if $gltfBuildings}
+	<T is={$gltfBuildings.scene} position={[10, 0, 0]} scale={scale.current} castShadow />
+{/if}
+
+{#if $gltfNeighborhood}
+	<T is={$gltfNeighborhood.scene} position={[10, 0, 0]} scale={scale.current} castShadow />
 {/if}
