0fa69b344fbab5d43d378804e66c29eda21c6c4d
Joel K Rivers <48956906+aTurbulentMind@users.noreply.github.com>
2025-06-10 09:40:35 -0700

    Add files via upload

---
+++ src/routes/World/LandSet.svelte
@@ -0,0 +1,57 @@
+<script>
+	import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+	import { T, useLoader, useTask } from '@threlte/core';
+	import { interactivity, ContactShadows } from '@threlte/extras';
+	import { Spring } from 'svelte/motion';
+
+	const gltfLand = useLoader(GLTFLoader).load('/src/lib/assets/models/World/HomeBoundLand.glb');
+	const gltfLandB = useLoader(GLTFLoader).load('/src/lib/assets/models/World/HomeBoundLandBot.glb');
+	const gltfStreet = useLoader(GLTFLoader).load('/src/lib/assets/models/World/HomeBoundStreet.glb');
+	const gltfParking = useLoader(GLTFLoader).load(
+		'/src/lib/assets/models/World/HomeBoundParking.glb'
+	);
+	const gltfTrail = useLoader(GLTFLoader).load('/src/lib/assets/models/World/HomeBoundTrail.glb');
+	const gltfExtras = useLoader(GLTFLoader).load('/src/lib/assets/models/World/HomeBoundExtras.glb');
+	const gltfBuildings = useLoader(GLTFLoader).load(
+		'/src/lib/assets/models/World/HomeBoundBuildings.glb'
+	);
+	const gltfNeighborhood = useLoader(GLTFLoader).load(
+		'/src/lib/assets/models/World/HomeBoundNeighborhood.glb'
+	);
+
+	interactivity();
+
+	const scale = new Spring(1);
+</script>
+
+{#if $gltfLand}
+	<T is={$gltfLand.scene} position={[10, 0, 0]} scale={scale.current} castShadow />
+{/if}
+
+{#if $gltfLandB}
+	<T is={$gltfLandB.scene} position={[10, 0, 0]} scale={scale.current} castShadow />
+{/if}
+
+{#if $gltfStreet}
+	<T is={$gltfStreet.scene} position={[10, 0, 0]} scale={scale.current} castShadow />
+{/if}
+
+{#if $gltfParking}
+	<T is={$gltfParking.scene} position={[10, 0, 0]} scale={scale.current} castShadow />
+{/if}
+
+{#if $gltfTrail}
+	<T is={$gltfTrail.scene} position={[10, 0, 0]} scale={scale.current} castShadow />
+{/if}
+
+{#if $gltfExtras}
+	<T is={$gltfExtras.scene} position={[10, 0, 0]} scale={scale.current} castShadow />
+{/if}
+
+{#if $gltfBuildings}
+	<T is={$gltfBuildings.scene} position={[10, 0, 0]} scale={scale.current} castShadow />
+{/if}
+
+{#if $gltfNeighborhood}
+	<T is={$gltfNeighborhood.scene} position={[10, 0, 0]} scale={scale.current} castShadow />
+{/if}
---
+++ src/routes/icons/EmailIcon.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+	import { T, useLoader, useTask } from '@threlte/core';
+	import { interactivity } from '@threlte/extras';
+	import { Spring } from 'svelte/motion';
+
+	const gltf = useLoader(GLTFLoader).load('./emailIcon.glb');
+
+	interactivity();
+
+	const scale = new Spring(1);
+
+	let rotation = $state(0);
+	useTask((delta) => {
+		rotation += delta / 6 / 20;
+	});
+</script>
+
+{#if $gltf}
+	<T is={$gltf.scene} position={[10, 0, 0]} scale={scale.current} castShadow />
+{/if}
