e4ce75694d0ab052bc58885543807f38004f3cde
Joel K Rivers <48956906+lanternLightDeveloper@users.noreply.github.com>
2025-07-15 12:43:31 -0700

    Add files via upload

---
+++ src/lib/styles/baseCamp.svelte
@@ -0,0 +1,9 @@
+<script>
+	import './animation.css'
+	import './colors.css'
+	import './components.css'
+	import './buttons.css'
+	import './generals.css'
+	import './vari.css'
+	import './util-First.css'
+</script>
