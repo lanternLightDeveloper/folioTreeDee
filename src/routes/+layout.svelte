b6f970116befa1eb767822759ba9e118b4d7d3a6
Joel K Rivers <48956906+lanternLightDeveloper@users.noreply.github.com>
2025-08-07 11:12:33 -0700

    Add files via upload

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
-	// import '../app.css';
+	import '../app.css';
 	import '$lib/styles/baseCamp.svelte';
+	import Header from './Header.svelte';
 	import { invalidate } from '$app/navigation';
 	import { onMount } from 'svelte';
 
 	let { data, children } = $props();
 	let { session, supabase } = $derived(data);
-	onMount(() => {
-		if (!supabase?.auth) return;
 
+	onMount(() => {
 		const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
 			if (newSession?.expires_at !== session?.expires_at) {
 				invalidate('supabase:auth');
@@ -20,5 +20,9 @@
 </script>
 
 <div class="app">
-	{@render children()}
+	<Header />
+	<main>
+		{@render children()}
+	</main>
+	<!-- <Footer />  -->
 </div>
